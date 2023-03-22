f32bf4035c3b99a07a61e35717f0d813fc4f12ba
amonnatt <117516530+amonnatt@users.noreply.github.com>
2023-03-22 14:17:40 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,4 +15,9 @@ def read_item(item_id: int, q: str = None):
 def read_name(name:str):
     return {"hello":name}
 
+@app.post("/hello")
+def read_name(name:str):
+    return {"hello":name}
+
+
 handler = Mangum(app)
